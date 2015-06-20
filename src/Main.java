673ef595220bac15f4e0525786e69987c1aa54f6
Eugene Levenetc <shumvlesu@gmail.com>
2015-06-20 18:23:06 +0300

    rx java samples

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import su.levenetc.algorithms.RXJava;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		int[] input = {0, 1, 2, 3};
+//		RotateArray rotateTest = new RotateArray(input);
+		//RotateArray.testWholeRotation(input, rotateTest.rotateWhole());
+//		Matrices.subtract();
+		//Matrices.shift();
+
+//		float distance = MathUtils.distance(100, 100);
+//		System.out.println(distance);
+//		RXJava.filter();
+//		RXJava.debounce();
+		RXJava.treads();
+	}
+}
---
+++ src/su/levenetc/algorithms/MathUtils.java
@@ -0,0 +1,10 @@
+package su.levenetc.algorithms;
+
+/**
+ * Created by elevenetc on 13/06/15.
+ */
+public class MathUtils {
+	public static float distance(float diffX, float diffY) {
+		return (float) Math.sqrt(diffX * diffX + diffY * diffY);
+	}
+}
