734265523cbb5333a122009cc845302d64e80193
Eugene Levenetc <shumvlesu@gmail.com>
2015-10-12 14:30:03 +0300

    Observable.cache

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,8 @@
-import su.levenetc.algorithms.strings.Strings;
+import su.levenetc.algorithms.rxjava.RXCaching;
 
 public class Main {
-	//v 1.0.1
-	public static void main(String[] args) {
-//		RXJava.checkNetworkConnection();
-//		Threads.checkSyncMethod();
-//		NumbersTests.run();
 
-//		Structures.run();
-		//Strings.cutSymbols();
-//		NumbersTests.isNumberFloatOrInteger();
-//		UseCachedData.run();
-//		LogErrorOnEveryError.run();
-		//some comment
-//		CheckComparator.testUsersStatuses();
-//		SmoothValue.run();
-//		SomeMath.sign();
-//		Operators.run();
-		Strings.trims();
+	public static void main(String[] args) {
+		RXCaching.run();
 	}
 }
---
+++ src/su/levenetc/algorithms/rxjava/RXCaching.java
@@ -0,0 +1,51 @@
+package su.levenetc.algorithms.rxjava;
+
+import rx.Observable;
+import rx.Subscriber;
+import su.levenetc.algorithms.utils.Out;
+
+/**
+ * Created by levenetc on 12/10/15.
+ */
+public class RXCaching {
+	public static void run() {
+		Observable<String> observable = Observable.create(new Observable.OnSubscribe<String>() {
+			@Override
+			public void call(Subscriber<? super String> subscriber) {
+				Out.pln("called");
+				subscriber.onStart();
+				subscriber.onError(new Exception("error"));
+				//subscriber.onNext(">");
+				subscriber.onCompleted();
+			}
+		}).cache();
+
+
+		observable.subscribe(RXCaching::handleResult, RXCaching::onError);
+		observable.subscribe(RXCaching::handleResult, RXCaching::onError);
+		observable.subscribe(new Subscriber<String>() {
+			@Override
+			public void onCompleted() {
+
+			}
+
+			@Override
+			public void onError(Throwable throwable) {
+				Out.pln("B:error:" + throwable.toString());
+			}
+
+			@Override
+			public void onNext(String result) {
+				Out.pln("B" + result);
+			}
+		});
+	}
+
+	private static void onError(Throwable throwable) {
+		Out.pln("A:error:" + throwable.toString());
+	}
+
+	private static void handleResult(String result) {
+		Out.pln("A" + result);
+	}
+}
