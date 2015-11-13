c02ade6c57015c1fdc6dba6bb0defca6f7d25140
Eugene Levenetc <shumvlesu@gmail.com>
2015-11-13 15:39:54 +0300

    Optimize imports in main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import rx.Observable;
 import su.levenetc.algorithms.rxjava.RXCast;
 import su.levenetc.algorithms.rxjava.RXConcatAndLast;
+import su.levenetc.algorithms.utils.Objects;
 
 public class Main {
 
