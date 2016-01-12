a920581e8f47838d201c777f28cafe4fd0441e59
Eugene Levenetc <shumvlesu@gmail.com>
2016-01-12 16:29:24 +0300

    Add String.format tests

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import su.levenetc.algorithms.threads.JoinSample;
-import su.levenetc.algorithms.utils.Time;
+import su.levenetc.algorithms.core.FormatString;
 
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-		Time.start();
-		JoinSample.run();
+		FormatString.run();
 	}
 }
