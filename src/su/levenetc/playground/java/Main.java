d825b62436042a121652c27d96a766b838cca73e
Eugene Levenetc <shumvlesu@gmail.com>
2016-02-06 04:38:27 +0300

    Balance brackets algorithm

--- src/su/levenetc/playground/java/Main.java
+++ src/su/levenetc/playground/java/Main.java
@@ -1,11 +1,11 @@
 package su.levenetc.playground.java;
 
 
-import su.levenetc.playground.java.algs.FlatArray;
+import su.levenetc.playground.java.algs.BalancingBrackets;
 
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-		FlatArray.run();
+		BalancingBrackets.run();
 	}
 }
