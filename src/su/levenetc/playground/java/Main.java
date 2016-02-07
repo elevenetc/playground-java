da5783a2ddce05683a1e70896f9712425a6b6296
Eugene Levenetc <shumvlesu@gmail.com>
2016-02-07 20:45:18 +0300

    SquareDuplicates

--- src/su/levenetc/playground/java/Main.java
+++ src/su/levenetc/playground/java/Main.java
@@ -1,11 +1,11 @@
 package su.levenetc.playground.java;
 
 
-import su.levenetc.playground.java.algs.ClearCopies;
+import su.levenetc.playground.java.algs.SquareDuplicates;
 
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-		ClearCopies.run();
+		SquareDuplicates.run();
 	}
 }
