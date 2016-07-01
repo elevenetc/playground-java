b94da0e2af98106cef6a9c4f2df63ccf2c1b5071
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-07-01 12:03:19 +0200

    Add RX utils

---
+++ src/su/levenetc/playground/java/rxjava/RXUtils.java
@@ -0,0 +1,77 @@
+package su.levenetc.playground.java.rxjava;
+
+import rx.Observable;
+import rx.Scheduler;
+import rx.Subscriber;
+import rx.schedulers.Schedulers;
+import su.levenetc.playground.java.utils.Out;
+import su.levenetc.playground.java.utils.ThreadsUtils;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by eugene.levenetc on 01/07/16.
+ */
+public class RXUtils {
+
+	private static Scheduler main;
+	private static Scheduler back;
+
+	public static <T> Observable<T> getObs(T result, String tag, long duration) {
+		return Observable.create(new Observable.OnSubscribe<T>() {
+			@Override
+			public void call(Subscriber<? super T> subscriber) {
+				Out.plnCurrentThread(tag);
+				ThreadsUtils.sleep(duration);
+				subscriber.onNext(result);
+				subscriber.onCompleted();
+			}
+		});
+	}
+
+	public static Scheduler getMain() {
+		if (main == null) main = getSingleThreadScheduler("Main");
+		return main;
+	}
+
+	public static Scheduler getBackground() {
+		if (back == null) back = getSingleThreadScheduler("Back");
+		return back;
+	}
+
+	public static Scheduler getMultiThreadScheduler(String name) {
+		ExecutorService executor = Executors.newFixedThreadPool(10, runnable -> {
+			Thread result = getThread(runnable, name);
+			return result;
+		});
+		return Schedulers.from(executor);
+	}
+
+	public static Scheduler getSingleThreadScheduler(String name) {
+		ExecutorService executor = Executors.newSingleThreadExecutor(runnable -> {
+			Thread result = getThread(runnable, name);
+			return result;
+		});
+		return Schedulers.from(executor);
+	}
+
+	private static Thread getThread(Runnable runnable, String name) {
+		Thread result = new Thread(runnable);
+		if (name.length() > 4) name = name.substring(0, 4);
+		result.setName("T: " + name);
+		return result;
+	}
+
+	public static <T> void onCompleteWithThread(T result) {
+		Out.pln(Out.currentThread() + " : onComplete: " + result);
+	}
+
+	public static <T> void onComplete(T result) {
+		Out.pln("onComplete: " + result);
+	}
+
+	public static void onError(Throwable t) {
+		Out.pln(t);
+	}
+}
