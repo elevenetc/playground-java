213a244ec7311f8ba4f1ba118f3e5c0a2f572925
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-07-01 12:52:48 +0200

    Refactor observable queue

--- src/su/levenetc/playground/java/rxjava/ObservableQueue.java
+++ src/su/levenetc/playground/java/rxjava/ObservableQueue.java
@@ -2,6 +2,7 @@ package su.levenetc.playground.java.rxjava;
 
 import rx.Observable;
 import rx.Scheduler;
+import rx.functions.Action0;
 import su.levenetc.playground.java.utils.Objects;
 import su.levenetc.playground.java.utils.Out;
 import su.levenetc.playground.java.utils.ThreadsUtils;
@@ -14,54 +15,64 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ObservableQueue {
 	public static void run() {
 		ObsQueue queue = new ObsQueue();
-		queue.add(RXUtils.getObs(new Objects.A(), "working - a", 200))
-				.observeOn(RXUtils.getMain())
-				.subscribe(RXUtils::onCompleteWithThread, RXUtils::onError);
 
-		queue.add(RXUtils.getObs(new Objects.A(), "working - b", 100))
-				.observeOn(RXUtils.getMain())
-				.subscribe(RXUtils::onCompleteWithThread, RXUtils::onError);
+		Action0 checkRes = () -> Out.pln("isResAvailable: " + queue.isResAvailable());
+
+		Observable<Objects.A> obsA = RX.getObs(new Objects.A("A"), "task A running", 200, false, checkRes);
+		Observable<Objects.A> obsB = RX.getObs(new Objects.A("B"), "task B running", 100, false, checkRes);
+		Observable<Objects.A> obsC = RX.getObs(new Objects.A("C"), "task C running", 350, false, checkRes);
+
+		queue.add(obsA)
+				.observeOn(RX.getMain())
+				.subscribe(RX::onCompleteWithThread, RX::onErrorWithThread);
+
+		queue.add(obsB)
+				.observeOn(RX.getMain())
+				.subscribe(RX::onCompleteWithThread, RX::onErrorWithThread);
+
+		queue.add(obsC)
+				.observeOn(RX.getMain())
+				.subscribe(RX::onCompleteWithThread, RX::onErrorWithThread);
 	}
 
 	private static class ObsQueue {
 
-		private AtomicInteger counter = new AtomicInteger();
 		private Object res;
-		private Scheduler internalScheduler = RXUtils.getSingleThreadScheduler("Internal");
+		private Scheduler internalScheduler = RX.getSingleThreadScheduler("Internal");
+		private AtomicInteger atomicInt = new AtomicInteger();
 
 		public <T> Observable<T> add(Observable<T> obs) {
+			atomicInt.incrementAndGet();
 			return obs
-					.doOnSubscribe(this::increment)
-					.doOnTerminate(this::decrement)
+					.doOnSubscribe(this::handleSubscription)
+					.doOnTerminate(this::handleTermination)
 					.subscribeOn(internalScheduler);
-		}
-
-		private void increment() {
-			Out.plnCurrentThread("increment");
-			counter.incrementAndGet();
-			if (!isResAvailable()) loadRes();
-		}
-
-		private void decrement() {
-			Out.plnCurrentThread("decrement");
-			if (counter.decrementAndGet() == 0) {
-				destroyRes();
-			}
 		}
 
 		private boolean isResAvailable() {
 			return res != null;
 		}
 
+		private void handleSubscription() {
+			if (!isResAvailable())
+				loadRes();
+		}
+
+		private void handleTermination() {
+			if (atomicInt.decrementAndGet() == 0 && isResAvailable()) {
+				destroyRes();
+			}
+		}
+
+		private void loadRes() {
+			Out.plnCurrentThread("res loading");
+			ThreadsUtils.sleep(200);
+			res = new Object();
+		}
+
 		private void destroyRes() {
 			Out.plnCurrentThread("res destroy");
 			res = null;
-		}
-
-		private Object loadRes() {
-			Out.plnCurrentThread("res loading");
-			ThreadsUtils.sleep(200);
-			return new Object();
 		}
 
 	}
