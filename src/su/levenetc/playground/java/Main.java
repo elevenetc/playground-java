5cefbb2b719f1afb861f0dfd17971693689bf419
Eugene Levenetc <shumvlesu@gmail.com>
2016-07-24 01:30:11 +0200

    Add SplitString: v0

--- src/su/levenetc/playground/java/Main.java
+++ src/su/levenetc/playground/java/Main.java
@@ -1,10 +1,10 @@
 package su.levenetc.playground.java;
 
-import su.levenetc.playground.java.rxjava.ObservableToCompletable;
+import su.levenetc.playground.java.problems.SplitString;
 
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
-		ObservableToCompletable.run();
+		SplitString.run();
 	}
 }
