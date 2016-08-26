0de41cdcbdda64dee5b0583c16892f257bd4c573
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-08-26 16:54:32 +0200

    Add string format replacement search function

--- src/su/levenetc/playground/java/lang/StringFormat.java
+++ src/su/levenetc/playground/java/lang/StringFormat.java
@@ -3,6 +3,10 @@ package su.levenetc.playground.java.lang;
 import su.levenetc.playground.java.utils.Out;
 
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static su.levenetc.playground.java.utils.TestUtils.assertEquals;
 
 /**
  * Created by eugene.levenetc on 26/08/16.
@@ -14,5 +18,44 @@ public class StringFormat {
         Out.pln(String.format("first: %1$d second: %2$s", 1, "a"));
         Out.pln(String.format("object: %1$s", new Object()));
         Out.pln(String.format("date: %1$tm.%1$te.%1$tY", new Date()));
+
+        checkFormat("%2$s %3$d", 2);
+        checkFormat("%1$d %2$s %3$d", 3);
+    }
+
+    private static void checkFormat(String formatA, int amount) {
+        Out.pln("'" + formatA + "': " + assertEquals(getAmountOfValidReplacements(formatA), amount));
+    }
+
+    private static boolean compareFormats(String formatA, String formatB) {
+        return false;
+    }
+
+    private static int amountOfReplacement(String format) {
+        int result = 0;
+        int lastIndex = 0;
+        while (lastIndex > -1) {
+            String check = "%";
+            lastIndex = format.indexOf(check, lastIndex);
+            if (lastIndex > -1) {
+                int length = check.length();
+                lastIndex += length;
+                result++;
+            }
+
+        }
+        return result;
+    }
+
+    private static int getAmountOfValidReplacements(String format) {
+        Pattern p = Pattern.compile("%\\d(\\$s|\\$d)");
+        Matcher m = p.matcher(format);
+        int result = 0;
+        while (m.find()) result++;
+        return result;
+    }
+
+    private static boolean isReplacementValid(String replacement) {
+        return false;
     }
 }
--- src/su/levenetc/playground/java/utils/TestUtils.java
+++ src/su/levenetc/playground/java/utils/TestUtils.java
@@ -6,19 +6,27 @@ import java.util.List;
  * Created by eleven on 25/01/2016.
  */
 public class TestUtils {
-	public static <T extends Comparable<? super T>> void testSort(List<T> check) {
-		Out.pln("Check:");
-		Out.pln(check);
-		Out.pln("Result:");
+    public static <T extends Comparable<? super T>> void testSort(List<T> check) {
+        Out.pln("Check:");
+        Out.pln(check);
+        Out.pln("Result:");
 
-		T prevValue = check.get(0);
-		for (int i = 1; i < check.size(); i++) {
-			if (check.get(i).compareTo(prevValue) < 0) {
-				throw new RuntimeException("Value " + check.get(i) + " at index " + i + " is lower than previous value: " + check.get(i - 1));
-			}
-			prevValue = check.get(i);
-		}
+        T prevValue = check.get(0);
+        for (int i = 1; i < check.size(); i++) {
+            if (check.get(i).compareTo(prevValue) < 0) {
+                throw new RuntimeException("Value " + check.get(i) + " at index " + i + " is lower than previous value: " + check.get(i - 1));
+            }
+            prevValue = check.get(i);
+        }
 
-		Out.pln("MergeSort ok with " + check);
-	}
+        Out.pln("MergeSort ok with " + check);
+    }
+
+    public static String assertEquals(Object a, Object b) {
+        boolean isEqual;
+        if (a == null && b == null) isEqual = true;
+        else if (a == null || b == null) isEqual = false;
+        else isEqual = a.equals(b);
+        return String.format("a: %1$s b: %2$s isEqual: %3$s", a, b, isEqual);
+    }
 }
