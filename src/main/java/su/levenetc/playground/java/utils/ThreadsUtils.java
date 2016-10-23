fdd74cadb13420f4e9e4fc8b3a5219563caa5e50
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-10-23 14:19:12 +0200

    Add multithread publish test

--- src/main/java/su/levenetc/playground/java/utils/ThreadsUtils.java
+++ src/main/java/su/levenetc/playground/java/utils/ThreadsUtils.java
@@ -1,9 +1,18 @@
 package su.levenetc.playground.java.utils;
 
+import io.reactivex.Scheduler;
+import io.reactivex.schedulers.Schedulers;
+
+import java.util.concurrent.Executors;
+
 /**
  * Created by elevenetc on 04/07/15.
  */
 public class ThreadsUtils {
+
+    public static Scheduler createScheduler(String name) {
+        return Schedulers.from(Executors.newSingleThreadExecutor(runnable -> new Thread(runnable, name)));
+    }
 
     public static void sleepA() {
         sleep(1000);
--- src/test/java/RxTests.java
+++ src/test/java/RxTests.java
@@ -1,7 +1,10 @@
+import io.reactivex.subjects.PublishSubject;
 import org.junit.Test;
 import rx.Observable;
 import rx.functions.Action0;
 import rx.subscriptions.Subscriptions;
+import su.levenetc.playground.java.utils.Out;
+import su.levenetc.playground.java.utils.ThreadsUtils;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -10,15 +13,42 @@ import static org.mockito.Mockito.verify;
  * Created by eleven on 02/10/2016.
  */
 public class RxTests {
-	@Test public void testUnsubHandler() {
+    @Test
+    public void testUnsubHandler() {
 
-		Action0 action = mock(Action0.class);
+        Action0 action = mock(Action0.class);
 
-		Observable.create(subscriber -> {
-			subscriber.onNext(new Object());
-			subscriber.add(Subscriptions.create(action));
-		}).subscribe().unsubscribe();
+        Observable.create(subscriber -> {
+            subscriber.onNext(new Object());
+            subscriber.add(Subscriptions.create(action));
+        }).subscribe().unsubscribe();
 
-		verify(action).call();
-	}
+        verify(action).call();
+    }
+
+    @Test
+    public void testMultiThreadPublish() {
+        PublishSubject<String> observable = PublishSubject.create();
+
+        observable
+                .observeOn(ThreadsUtils.createScheduler("Thread:A"))
+                .subscribe(s -> {
+                    Out.pln("Consumer:A:start:" + s);
+                    Out.plnCurrentThread();
+                    Out.pln("Consumer:A:end:" + s);
+                });
+
+        observable
+                .observeOn(ThreadsUtils.createScheduler("Thread:B"))
+                .subscribe(s -> {
+                    Out.pln("Consumer:B:start:" + s);
+                    Out.plnCurrentThread();
+                    Out.pln("Consumer:B:end:" + s);
+                });
+
+        new Thread(() -> observable.onNext("X"), "ProducerThread").start();
+
+
+        ThreadsUtils.sleep(500);
+    }
 }
