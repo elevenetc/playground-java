66deea6038909dbb76c466904f725b43c4d29d47
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-10-28 12:41:36 +0200

    Fix cipher tests

--- src/main/java/su/levenetc/playground/java/security/ValueCipher.java
+++ src/main/java/su/levenetc/playground/java/security/ValueCipher.java
@@ -1,9 +1,13 @@
 package su.levenetc.playground.java.security;
 
+
 import javax.crypto.*;
 import java.nio.ByteBuffer;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+
+import static java.util.Base64.getDecoder;
+import static java.util.Base64.getEncoder;
 
 /**
  * Created by eugene.levenetc on 27/10/2016.
@@ -18,18 +22,24 @@ public class ValueCipher {
         this.alg = alg;
     }
 
-    public int encrypt(int value) {
-        final byte[] array = fromInt(value);
-        final byte[] encryptedByteArray = encrypt(array, keyProvider.getKey(), alg);
-        final int encryptedInt = fromByteArray(encryptedByteArray);
-        return encryptedInt;
+    public byte[] encrypt(byte[] value) {
+        return encrypt(value, keyProvider.getKey(), alg);
     }
 
-    public int decrypt(int value) {
-        final byte[] byteArray = fromInt(value);
-        final byte[] decryptedByteArray = decrypt(byteArray, keyProvider.getKey(), alg);
-        final int decryptedInt = fromByteArray(decryptedByteArray);
-        return decryptedInt;
+    public byte[] decrypt(byte[] value) {
+        return decrypt(value, keyProvider.getKey(), alg);
+    }
+
+    public String encrypt(String value) {
+        final byte[] encrypted = encrypt(getEncoder().encode(value.getBytes()), keyProvider.getKey(), alg);
+        final String encoded = getEncoder().encodeToString(encrypted);
+        return encoded;
+    }
+
+    public String decrypt(String value) {
+        final byte[] decoded = getDecoder().decode(value);
+        final byte[] decrypted = decrypt(decoded, keyProvider.getKey(), alg);
+        return new String(getDecoder().decode(decrypted));
     }
 
     private static byte[] encrypt(byte[] value, SecretKey secret, String cipherAlg) {
--- src/test/java/TestCipher.java
+++ src/test/java/TestCipher.java
@@ -1,25 +1,39 @@
+import org.junit.Before;
 import org.junit.Test;
 import su.levenetc.playground.java.security.ValueCipher;
 import su.levenetc.playground.java.utils.StringUtils;
 
-import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by eugene.levenetc on 27/10/2016.
  */
 public class TestCipher {
-    @Test
-    public void testInt() {
+    
+    private ValueCipher valueCipher;
+
+    @Before
+    public void before() {
         final String password = StringUtils.getRandomString(16);
-        final ValueCipher valueCipher = new ValueCipher(new ValueCipher.KeyProvider() {
-            @Override
-            public SecretKey getKey() {
-                return new SecretKeySpec(password.getBytes(), "AES");
-            }
-        }, "AES/CBC/PKCS5Padding");
-        final int input = 100;
-        final int encrypted = valueCipher.encrypt(input);
-        final int decrypted = valueCipher.decrypt(encrypted);
+        valueCipher = new ValueCipher(() -> new SecretKeySpec(password.getBytes(), "AES"), "AES/ECB/PKCS5Padding");
+    }
+
+    @Test
+    public void testByteArray() {
+        final byte[] input = {1, 2, 3, 4};
+        final byte[] encrypted = valueCipher.encrypt(input);
+        final byte[] decrypted = valueCipher.decrypt(encrypted);
+        assertArrayEquals(input, decrypted);
+    }
+
+    @Test
+    public void testString() {
+        final String input = "hello";
+        final String encrypted = valueCipher.encrypt(input);
+        final String decrypted = valueCipher.decrypt(encrypted);
+        assertEquals(input, decrypted);
     }
 }
