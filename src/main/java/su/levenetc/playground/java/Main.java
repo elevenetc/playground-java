df4d0ea09b014675e906fe025aed94557ddc4e86
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-12-16 18:20:09 +0100

    Add sonar config

--- src/main/java/su/levenetc/playground/java/Main.java
+++ src/main/java/su/levenetc/playground/java/Main.java
@@ -1,32 +1,11 @@
 package su.levenetc.playground.java;
 
-import su.levenetc.playground.java.bot.HellBot;
-import su.levenetc.playground.java.bot.models.User;
-import su.levenetc.playground.java.bot.platforms.slack.SlackMoshiParser;
-import su.levenetc.playground.java.bot.platforms.slack.SlackPlatform;
-import su.levenetc.playground.java.bot.services.CurrentRunningService;
-import su.levenetc.playground.java.bot.services.HeavyService;
-import su.levenetc.playground.java.bot.services.PingService;
-import su.levenetc.playground.java.bot.services.SimpleSurveyService;
-import su.levenetc.playground.java.bot.services.simpleconversation.SimpleConversation;
-import su.levenetc.playground.java.bot.wws.OkHttpSocketClient;
-import su.levenetc.playground.java.utils.ThreadsUtils;
+import su.levenetc.playground.java.algs.pathfinding.GraphUtils;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        new HellBot.Builder()
-                .setPlatform(new SlackPlatform(new OkHttpSocketClient(), new SlackMoshiParser(), System.getenv("SLACK_TOKEN")))
-                .addService(new HeavyService())
-                .addService(new PingService())
-                .addService(new CurrentRunningService())
-                .addService(new SimpleConversation())
-                .addService(new SimpleSurveyService())
-                .setDebugUsers(new User("U04CXNETW"))
-                .setMode(HellBot.Mode.DEBUG)
-                .build()
-                .start()
-                .subscribe();
-        ThreadsUtils.sleepForever();
+        GraphUtils.printGraph(GraphUtils.genEmpty());
     }
+
 }
