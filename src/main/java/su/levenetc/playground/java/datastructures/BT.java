173718bf1cfe551645eca444ac12f7d0b6f4f47f
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-02-09 01:04:48 +0100

    Add bs

--- src/main/java/su/levenetc/playground/java/datastructures/BT.java
+++ src/main/java/su/levenetc/playground/java/datastructures/BT.java
@@ -1,5 +1,7 @@
 package su.levenetc.playground.java.datastructures;
 
+import su.levenetc.playground.java.utils.Out;
+
 /**
  * Created by eugene.levenetc on 08/02/2017.
  */
@@ -7,12 +9,13 @@ public class BT {
 
     Node root;
 
-    public void add(int value) {
+    public BT add(int value) {
         if (root == null) {
             root = new Node(value);
         } else {
             internalAdd(root, value);
         }
+        return this;
     }
 
     public Node getRoot() {
@@ -20,20 +23,77 @@ public class BT {
     }
 
     private void internalAdd(Node node, int value) {
+        if (value < node.value) {
+            if (node.left == null) {
+                node.left = new Node(value);
+            } else {
+                internalAdd(node.left, value);
+            }
+        } else {
+            if (node.right == null) {
+                node.right = new Node(value);
+            } else {
+                internalAdd(node.right, value);
+            }
+        }
+    }
 
-        if (node.left != null && value < node.left.value) {
-            internalAdd(node.left, value);
-            return;
-        } else if (node.right != null && value > node.right.value) {
-            internalAdd(node.right, value);
-            return;
+    public void printPostOrder() {
+        printPostOrder(root);
+    }
+
+    public void printInOrder() {
+        printInOrder(root);
+    }
+
+    public int minValue() {
+        return minValue(root, root.value);
+    }
+
+    public int getDepth() {
+        if (root == null) return 0;
+        return getDepth(root, 0);
+    }
+
+    private int minValue(Node node, int minValue) {
+        if (node == null) {
+            return minValue;
+        } else {
+            return minValue(node.left, node.value);
+        }
+    }
+
+    private void printPostOrder(Node node) {
+        if (node.left != null) printPostOrder(node.left);
+        if (node.right != null) printPostOrder(node.right);
+        Out.pln(node.value);
+    }
+
+    private void printInOrder(Node node) {
+        if (node.left != null) printInOrder(node.left);
+        Out.pln(node.value);
+        if (node.right != null) printInOrder(node.right);
+    }
+
+    private int getDepth(BT.Node node, int depth) {
+
+        int left = 0;
+        int right = 0;
+
+        if (node.left != null) {
+            left = getDepth(node.left, ++depth);
         }
 
-        if (value > node.value) {
-            node.right = new Node(value);
-        } else if (value < node.value) {
-            node.left = new Node(value);
+        if (node.right != null) {
+            right = getDepth(node.right, ++depth);
         }
+
+        if (left == 0 && right == 0) {
+            return depth;
+        } else {
+            return Math.max(left, right);
+        }
+
     }
 
     public static class Node {
--- src/test/java/TestBinaryTree.java
+++ src/test/java/TestBinaryTree.java
@@ -1,5 +1,5 @@
+import org.junit.Before;
 import org.junit.Test;
-import su.levenetc.playground.java.datastructures.BST2;
 import su.levenetc.playground.java.datastructures.BT;
 
 import static org.junit.Assert.assertEquals;
@@ -8,41 +8,48 @@ import static org.junit.Assert.assertEquals;
  * Created by eugene.levenetc on 06/02/2017.
  */
 public class TestBinaryTree {
-    @Test
-    public void testPathSum() {
-        BST2<Integer, String> bst = new BST2<>(10, "Top");
-        bst.put(1, "1");
-        bst.put(2, "2");
 
+    private BT bt;
+
+    @Before
+    public void before() {
+        bt = new BT();
     }
 
     @Test
     public void maxDepth() {
-        BT bt = new BT();
         bt.add(0);
         bt.add(1);
+        bt.add(-1);
         bt.add(2);
         bt.add(3);
 
-        assertEquals(3, goDown(bt.getRoot(), 0));
+        assertEquals(4, bt.getDepth());
     }
 
-    private int goDown(BT.Node node, int depth) {
+    @Test
+    public void testMin() {
+        bt.add(0);
+        bt.add(-1);
+        bt.add(-10);
+        bt.add(2);
+        bt.add(4);
+        bt.add(6);
 
-        int left = 0;
-        int right = 0;
-
-        if (node.left != null) {
-            left = goDown(node.left, ++depth);
-        } else if (node.right != null) {
-            right = goDown(node.right, ++depth);
-        }
-
-        if (left == 0 && right == 0) {
-            return depth;
-        } else {
-            return left > right ? left : right;
-        }
-
+        assertEquals(-10, bt.minValue());
     }
+
+    @Test
+    public void printInOrder() {
+        bt.add(-1).add(0).add(-3).add(-2).add(1).add(3).add(2).add(3);
+        bt.printInOrder();
+    }
+
+    @Test
+    public void printPostOrder() {
+        bt.add(-1).add(0).add(1).add(3).add(-2);
+        bt.printPostOrder();
+    }
+
+
 }
