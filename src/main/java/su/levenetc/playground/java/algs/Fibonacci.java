2839c5ecba1e1ebb27bd2797b648796d10fb865e
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-03-12 21:01:39 +0100

    Add big dec fibonacci

--- src/main/java/su/levenetc/playground/java/algs/Fibonacci.java
+++ src/main/java/su/levenetc/playground/java/algs/Fibonacci.java
@@ -1,9 +1,12 @@
 package su.levenetc.playground.java.algs;
 
+import java.math.BigDecimal;
+
 /**
  * Created by eugene.levenetc on 12/03/2017.
  */
 public class Fibonacci {
+
 
     public static class Iterative implements FibFun {
         private int n;
@@ -26,6 +29,28 @@ public class Fibonacci {
         }
     }
 
+    public static class BigDecFormula implements FibFun {
+        private int n;
+
+        public BigDecFormula(int n) {
+            this.n = n;
+        }
+
+        @Override
+        public long calc() {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            if (n == 2) return 1;
+            if (n == 3) return 2;
+            if (n == 4) return 3;
+            final BigDecimal two = BigDecimal.valueOf(2);
+            final BigDecimal square = BigDecimal.valueOf(Math.sqrt(5));
+            final BigDecimal pow1 = BigDecimal.ONE.add(square).divide(two, BigDecimal.ROUND_HALF_UP).pow(n);
+            final BigDecimal pow2 = BigDecimal.ONE.min(square).divide(two, BigDecimal.ROUND_HALF_UP).pow(n);
+            return pow1.add(pow2).divide(square, BigDecimal.ROUND_HALF_UP).longValue();
+        }
+    }
+
     public static class Formula implements FibFun {
         private int n;
 
@@ -35,10 +60,15 @@ public class Fibonacci {
 
         @Override
         public long calc() {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            if (n == 2) return 1;
+            if (n == 3) return 2;
+            if (n == 4) return 3;
             final double square = Math.sqrt(5);
             final double pow1 = Math.pow((1 + square) / 2, n);
             final double pow2 = Math.pow((1 - square) / 2, n);
-            return (long) ((pow1 + pow2) / square);
+            return Math.round((pow1 + pow2) / square);
         }
     }
 
--- src/test/java/TestFibonacci.java
+++ src/test/java/TestFibonacci.java
@@ -3,24 +3,69 @@ import su.levenetc.playground.java.algs.Fibonacci;
 import su.levenetc.playground.java.utils.Out;
 
 import java.math.BigInteger;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by eugene.levenetc on 12/03/2017.
  */
 public class TestFibonacci {
-    @Test
-    public void fib() {
 
-        final int n = 10;
+    private static final HashMap<Long, Long> numbers = new HashMap<Long, Long>() {{
+        put(0L, 0L);
+        put(1L, 1L);
+        put(2L, 1L);
+        put(3L, 2L);
+        put(4L, 3L);
+        put(5L, 5L);
+        put(6L, 8L);
+        put(7L, 13L);
+        put(8L, 21L);
+        put(9L, 34L);
+        put(25L, 75025L);
+    }};
+
+    @Test
+    public void testBigDecFormula() {
+        for (long n : numbers.keySet()) {
+            final long fibValue = numbers.get(n);
+            assertEquals(fibValue, new Fibonacci.BigDecFormula((int) n).calc());
+        }
+    }
+
+    @Test
+    public void testFormula() {
+        for (long n : numbers.keySet()) {
+            final long fibValue = numbers.get(n);
+            assertEquals(fibValue, new Fibonacci.Formula((int) n).calc());
+        }
+    }
+
+    @Test
+    public void testIterative() {
+        for (long n : numbers.keySet()) {
+            final long fibValue = numbers.get(n);
+            assertEquals(fibValue, new Fibonacci.Iterative((int) n).calc());
+        }
+    }
+
+    @Test
+    public void testSpeed() {
+
+        final int n = 200;
         final int times = 10000;
         final Fibonacci.Formula formula = new Fibonacci.Formula(n);
         final Fibonacci.Iterative iterative = new Fibonacci.Iterative(n);
+        final Fibonacci.BigDecFormula bigDecFormula = new Fibonacci.BigDecFormula(n);
 
+        Out.pln("Fib: " + n + ", times: " + times);
         Out.pln("Formula: " + checkFun(formula, times));
+        Out.pln("FormulaBigDec: " + checkFun(bigDecFormula, times));
         Out.pln("Iterative: " + checkFun(iterative, times));
     }
 
-    private BigInteger checkFun(Fibonacci.FibFun fun, int t){
+    private BigInteger checkFun(Fibonacci.FibFun fun, int t) {
         long[] times = new long[t];
         BigInteger result = BigInteger.valueOf(0);
 
