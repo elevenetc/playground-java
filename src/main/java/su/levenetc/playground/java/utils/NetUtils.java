2015ef8d2e2e0408c28b5be93f764c4ce90b79d2
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-03-22 17:41:20 +0100

    update NetUtils

--- src/main/java/su/levenetc/playground/java/utils/NetUtils.java
+++ src/main/java/su/levenetc/playground/java/utils/NetUtils.java
@@ -3,8 +3,9 @@ package su.levenetc.playground.java.utils;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.*;
+import java.net.*;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -28,4 +29,51 @@ public class NetUtils {
 
 		return null;
 	}
+
+    public static String executePost(String targetURL, String bodyParams) throws IOException {
+
+        URL url = new URL(targetURL);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        connection.setRequestProperty("Content-Length", Integer.toString(bodyParams.getBytes().length));
+        connection.setRequestProperty("Content-Language", "en-US");
+        connection.setUseCaches(false);
+        connection.setDoOutput(true);
+
+        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+        wr.writeBytes(bodyParams);
+        wr.close();
+
+        InputStream is = connection.getInputStream();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+        StringBuilder response = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            response.append(line);
+            response.append('\r');
+        }
+        rd.close();
+
+        connection.disconnect();
+
+        return response.toString();
+
+    }
+
+    public static void createSocketServer(int port) throws IOException {
+
+        Socket socket = null;
+        boolean end = true;
+
+        ServerSocket listener = new ServerSocket(port);
+        while (end) {
+            socket = listener.accept();
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(new Date().toString());
+        }
+
+        socket.close();
+        listener.close();
+    }
 }
