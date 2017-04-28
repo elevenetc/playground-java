cf7acc20f6a7e8ac499c7b38d319656c863ed292
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-04-28 02:28:58 +0200

    Update crossword

--- src/main/java/su/levenetc/playground/java/datastructures/Crossword.java
+++ src/main/java/su/levenetc/playground/java/datastructures/Crossword.java
@@ -1,5 +1,7 @@
 package su.levenetc.playground.java.datastructures;
 
+import su.levenetc.playground.java.utils.Utils;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -17,6 +19,48 @@ public class Crossword {
         public void cross(Letter letterA, Letter letterB) {
             letterA.cross = letterB;
             letterB.cross = letterA;
+        }
+
+        public void set(String... rows) {
+
+            String[] columns = new String[rows.length];
+            Queue<PlaceHolder> horiz = new LinkedList<>();
+            Queue<PlaceHolder> vert = new LinkedList<>();
+
+            for (int y = 0; y < rows.length; y++) {
+                String row = rows[y];
+
+                parsePlaceholder(row, horiz, y, true);
+
+                //build columns
+                for (int colInd = 0; colInd < row.length(); colInd++) {
+                    final char ch = row.charAt(colInd);
+                    if (columns[colInd] == null) {
+                        columns[colInd] = String.valueOf(ch);
+                    } else {
+                        columns[colInd] += String.valueOf(ch);
+                    }
+                }
+            }
+
+            for (int i = 0; i < columns.length; i++) {
+                parsePlaceholder(columns[i], vert, i, false);
+            }
+
+            for (PlaceHolder h : horiz) {
+                for (PlaceHolder v : vert) {
+                    final int[] intersection = h.intersection(v);
+                    if (intersection != null) {
+                        Letter letterA = v.getLetter(intersection[0], intersection[1]);
+                        Letter letterB = h.getLetter(intersection[0], intersection[1]);
+                        cross(letterA, letterB);
+                    }
+                }
+            }
+
+            horiz.addAll(vert);
+
+            set(horiz);
         }
 
         public void set(PlaceHolder... placeHolders) {
@@ -48,6 +92,7 @@ public class Crossword {
             String word = words.poll();
 
             for (PlaceHolder ph : phs) {
+
                 if (ph.isEmpty() && ph.isFit(word) && ph.set(word)) {
 
                     if (internalFill(words)) {
@@ -56,11 +101,37 @@ public class Crossword {
 
                     ph.unset();
                 }
+
             }
 
             words.add(word);
             return false;
 
+        }
+
+        private static void setCrossLetters(Queue<PlaceHolder> horiz, Queue<PlaceHolder> vert) {
+            for (PlaceHolder hPh : horiz) {
+                for (PlaceHolder vPh : vert) {
+
+                }
+            }
+        }
+
+        private static void parsePlaceholder(String line, Queue<PlaceHolder> placeholders, int y, boolean isHorisontal) {
+            if (line.contains("--")) {
+                int x = line.indexOf('-');
+                String rawPh = line.replaceAll("\\+", "");
+                PlaceHolder ph;
+                if (isHorisontal) {
+                    ph = new PlaceHolder(x, y, rawPh.length());
+                } else {
+                    ph = new PlaceHolder(y, x, rawPh.length());
+                }
+
+                ph.isHorizontal = isHorisontal;
+                ph.buildCoordinates();
+                placeholders.add(ph);
+            }
         }
     }
 
@@ -68,13 +139,49 @@ public class Crossword {
 
         List<Letter> letters;
         int length;
+        int x;
+        int y;
+        boolean isHorizontal;
 
+        public PlaceHolder(int x, int y, int length, boolean isHorizontal) {
+            this(x, y, length);
+            this.isHorizontal = isHorizontal;
+        }
+
+        public PlaceHolder(int x, int y, int length) {
+            this(length);
+            this.x = x;
+            this.y = y;
+        }
 
         public PlaceHolder(int length) {
             this.length = length;
             letters = new ArrayList<>(length);
             for (int i = 0; i < length; i++) {
                 letters.add(new Letter());
+            }
+        }
+
+        public void buildCoordinates() {
+            for (int i = 0; i < letters.size(); i++) {
+                final Letter letter = letters.get(i);
+                if (isHorizontal) {
+                    letter.x = x + i;
+                    letter.y = y;
+                } else {
+                    letter.x = x;
+                    letter.y = y + i;
+                }
+            }
+        }
+
+        public Letter getLetter(int x, int y) {
+            if (isHorizontal) {
+                if (this.y != y) return null;
+                return Utils.get(letters, x - this.x);
+            } else {
+                if (this.x != x) return null;
+                return Utils.get(letters, y - this.y);
             }
         }
 
@@ -119,6 +226,28 @@ public class Crossword {
             return true;
         }
 
+        public int[] intersection(PlaceHolder ph) {
+            if (ph.isHorizontal != isHorizontal) {
+
+                PlaceHolder horiz = isHorizontal ? this : ph;
+                PlaceHolder vert = isHorizontal ? ph : this;
+                boolean isXOk = horiz.x <= vert.x && horiz.x + length >= vert.x;
+                boolean isYOk = vert.y <= horiz.y && vert.y + length >= horiz.y;
+
+                if (isXOk && isYOk) {
+                    int x = vert.x;
+                    int y = horiz.y;
+                    return new int[]{x, y};
+                } else {
+                    return null;
+                }
+
+
+            } else {
+                return null;
+            }
+        }
+
         public void unset() {
             for (Letter letter : letters) {
                 if (!letter.isCross()) {
@@ -138,8 +267,14 @@ public class Crossword {
     }
 
     public static class Letter {
+
+        public int x, y;
         char ch = (char) -1;
         Letter cross;
+
+        public char getChar() {
+            return ch;
+        }
 
         boolean isCross() {
             return cross != null;
--- src/test/java/FillCrossword.java
+++ src/test/java/FillCrossword.java
@@ -2,7 +2,8 @@ import org.junit.Test;
 import su.levenetc.playground.java.datastructures.Crossword.Graph;
 import su.levenetc.playground.java.datastructures.Crossword.PlaceHolder;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static su.levenetc.playground.java.utils.MathUtils.ints;
 
 /**
  * Created by eugene.levenetc on 27/04/2017.
@@ -36,11 +37,53 @@ public class FillCrossword {
         graph.set(phABC, phAB, phZC);
         graph.cross(phABC.getLetter(0), phAB.getLetter(0));
         graph.cross(phABC.getLetter(2), phZC.getLetter(1));
-        
+
         graph.fill("abc", "ab", "zc");
 
         assertEquals("abc", phABC.toString());
         assertEquals("ab", phAB.toString());
         assertEquals("zc", phZC.toString());
+    }
+
+    @Test
+    public void fillCrossword03() {
+        Graph graph = new Graph();
+        String[] words = new String[]{
+                "---",
+                "-++",
+                "-++"
+        };
+        graph.set(words);
+        graph.fill("abc", "abc");
+    }
+
+    @Test
+    public void testIntersection() {
+        assertArrayEquals(ints(2, 2), new PlaceHolder(0, 2, 3, true)
+                .intersection(new PlaceHolder(2, 0, 4, false)));
+
+        assertArrayEquals(ints(1, 1), new PlaceHolder(0, 1, 3, true)
+                .intersection(new PlaceHolder(1, 0, 3, false)));
+
+        assertNull(new PlaceHolder(0, 10, 3, true)
+                .intersection(new PlaceHolder(2, 0, 4, false)));
+    }
+
+    @Test
+    public void testLetterGetter01() {
+        final PlaceHolder ph = new PlaceHolder(0, 2, 3, true);
+        ph.set("abc");
+        assertEquals('c', ph.getLetter(2, 2).getChar());
+        assertEquals('b', ph.getLetter(1, 2).getChar());
+    }
+
+    @Test
+    public void testLetterGetter02() {
+        final PlaceHolder ph = new PlaceHolder(3, 3, 3, false);
+        ph.set("abc");
+        assertEquals('a', ph.getLetter(3, 3).getChar());
+        assertEquals('b', ph.getLetter(3, 4).getChar());
+        assertEquals('c', ph.getLetter(3, 5).getChar());
+        assertNull(ph.getLetter(3, 6));
     }
 }
