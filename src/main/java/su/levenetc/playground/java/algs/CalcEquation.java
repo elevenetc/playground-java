e228aed3cf28e2b5a46c1e1991d6250a4a39d996
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-06-11 00:09:11 +0200

    Update calc equation

--- src/main/java/su/levenetc/playground/java/algs/CalcEquation.java
+++ src/main/java/su/levenetc/playground/java/algs/CalcEquation.java
@@ -1,65 +1,44 @@
 package su.levenetc.playground.java.algs;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
-
-import su.levenetc.playground.java.utils.Out;
 
 /**
  * Created by eugene.levenetc on 10/06/2017.
  */
 public class CalcEquation {
 
-    public static int calc(int a, int b, int c, int d) {
-
-        int result = 0;
-        int x;
-        int y;
-        Map<Double, Integer> results = new TreeMap<>();
-
-        for (int ic = 1; ic <= c; ic++) {
-            x = ic;
-            results.put(Math.pow(x, 2) - x * a, ic);
-        }
-
-        for (int id = 1; id <= d; id++) {
-
-            y = id;
-
-            if (results.containsKey(y * b - Math.pow(y, 2))) {
-                //results.put(right, 2);
-                result++;
-            }
-
-        }
-
-        return result;
-    }
-
     public static int calc2(int a, int b, int c, int d) {
 
         int result = 0;
         int x;
         int y;
+        Map<Double, Integer> results = new HashMap<>();
 
         for (int ic = 1; ic <= c; ic++) {
 
             x = ic;
             int xa = x * a;
+            double powX = Math.pow(x, 2);
+            double left = powX - xa;
 
-            for (int id = 1; id <= d; id++) {
-
-                y = id;
-
-                double left = Math.pow(x, 2) + Math.pow(y, 2);
-                double right = xa + y * b;
-
-                if (left == right) {
-                    Out.pln(String.format("x:%d y:%d=%f", x, y, left));
-                    result++;
-                }
+            if (results.containsKey(left)) {
+                results.put(left, results.get(left) + 1);
+            } else {
+                results.put(left, 1);
             }
         }
+
+        for (int id = 1; id <= d; id++) {
+
+            y = id;
+            double right = y * b - Math.pow(y, 2);
+
+            if (results.containsKey(right)) {
+                result += results.get(right);
+            }
+        }
+
 
         return result;
     }
--- src/test/java/CalcEquationTests.java
+++ src/test/java/CalcEquationTests.java
@@ -8,18 +8,13 @@ import static org.junit.Assert.assertEquals;
  * Created by eugene.levenetc on 10/06/2017.
  */
 public class CalcEquationTests {
+
     @Test
     public void test01() {
-        //assertEquals(1, (CalcEquation.calc(1, 1, 1, 1)));
-//        assertEquals(3, (CalcEquation.calc(10, 12, 50, 50)));
-//        assertEquals(3, (CalcEquation.calc(10, 23, 50, 50)));
+        assertEquals(1, (CalcEquation.calc2(1, 1, 1, 1)));
+        assertEquals(3, (CalcEquation.calc2(10, 12, 50, 50)));
+        assertEquals(3, (CalcEquation.calc2(10, 23, 50, 50)));
         assertEquals(2, (CalcEquation.calc2(51, 11, 50, 50)));
-        assertEquals(2, (CalcEquation.calc(51, 11, 50, 50)));
-//        assertEquals(0, (CalcEquation.calc(51, 11, 19, 50)));
-    }
-
-    @Test
-    public void test02() {
-
+        assertEquals(0, (CalcEquation.calc2(51, 11, 19, 50)));
     }
 }
