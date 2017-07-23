d8bb5478eb6848830ecc2e88570a66c0673cc896
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-07-23 22:51:39 +0200

    Add max sum at sell with revenue

--- src/main/java/su/levenetc/playground/java/algs/SellWithMaxRevenue.java
+++ src/main/java/su/levenetc/playground/java/algs/SellWithMaxRevenue.java
@@ -1,7 +1,7 @@
 package su.levenetc.playground.java.algs;
 
 public class SellWithMaxRevenue {
-    public static int get(int[] prices) {
+    public static int getOneMax(int[] prices) {
         int max = -1;
         for (int i = 0; i < prices.length - 1; i++) {
             final int buy = prices[i];
@@ -13,5 +13,38 @@ public class SellWithMaxRevenue {
             }
         }
         return max;
+    }
+
+    public static int getMaxSum(int[] prices) {
+
+        int buyIndex = 0;
+        int current = 1;
+        int result = 0;
+
+        while (current < prices.length) {
+
+            boolean isLast = current == prices.length - 1;
+
+            if (prices[current] < prices[buyIndex]) {
+                buyIndex = current;
+                current++;
+            } else if (prices[current] > prices[buyIndex]) {
+                if (!isLast) {
+                    final int future = prices[current + 1];
+                    if (future < prices[current]) {
+                        result += prices[current] - prices[buyIndex];
+                        buyIndex = current + 1;
+                        current = current + 2;
+                    } else {
+                        current++;
+                    }
+                } else {
+                    result += prices[current] - prices[buyIndex];
+                    break;
+                }
+            }
+        }
+
+        return result;
     }
 }
--- src/test/java/SellWithMaxRevenueTests.java
+++ src/test/java/SellWithMaxRevenueTests.java
@@ -1,18 +1,44 @@
 import org.junit.Test;
 
 import su.levenetc.playground.java.algs.SellWithMaxRevenue;
-import su.levenetc.playground.java.utils.Utils;
 
 import static org.junit.Assert.assertEquals;
+import static su.levenetc.playground.java.utils.Utils.array;
 
 public class SellWithMaxRevenueTests {
     @Test
     public void test01() {
-        assertEquals(5, SellWithMaxRevenue.get(Utils.array(5, 10)));
+        assertEquals(5, SellWithMaxRevenue.getOneMax(array(5, 10)));
     }
 
     @Test
     public void test02() {
-        assertEquals(17, SellWithMaxRevenue.get(Utils.array(5, 10, 3, 11, 20)));
+        assertEquals(17, SellWithMaxRevenue.getOneMax(array(5, 10, 3, 11, 20)));
     }
+
+    @Test
+    public void test03() {
+        assertEquals(50, SellWithMaxRevenue.getMaxSum(array(50, 100)));
+    }
+
+    @Test
+    public void test04() {
+        assertEquals(95, SellWithMaxRevenue.getMaxSum(array(50, 100, 10, 5, 50)));
+    }
+
+    @Test
+    public void test05() {
+        assertEquals(865, SellWithMaxRevenue.getMaxSum(array(100, 180, 260, 310, 40, 535, 695)));
+    }
+
+    @Test
+    public void test06() {
+        assertEquals(100, SellWithMaxRevenue.getMaxSum(array(50, 100, 150)));
+    }
+
+    @Test
+    public void test07() {
+        assertEquals(60, SellWithMaxRevenue.getMaxSum(array(50, 100, 10, 20)));
+    }
+
 }
