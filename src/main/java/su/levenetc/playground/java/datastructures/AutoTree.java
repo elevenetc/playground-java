d9f6b7c39c9d647bd28cab44375c12f5e2ad14e6
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-08-02 12:26:08 +0200

    Add autocomplete tree

---
+++ src/main/java/su/levenetc/playground/java/datastructures/AutoTree.java
@@ -0,0 +1,84 @@
+package su.levenetc.playground.java.datastructures;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AutoTree {
+
+    private List<String> variants;
+    private AutoTree next;
+    private AutoTree prev;
+    private boolean optional;
+
+    public AutoTree(String... variants) {
+        this.variants = Arrays.asList(variants);
+    }
+
+    public AutoTree next() {
+        return next;
+    }
+
+    public AutoTree isCompletable(String value) {
+        char[] toComplete = value.toCharArray();
+        for (String variant : variants) {
+            char[] variantChars = variant.toCharArray();
+            if (toComplete.length > variantChars.length) continue;
+            boolean invalid = false;
+            for (int i = 0; i < toComplete.length; i++) {
+                char ch = toComplete[i];
+                if (ch != variantChars[i]) {
+                    invalid = true;
+                    break;
+                }
+            }
+            if (invalid) continue;
+            return next;
+        }
+        return null;
+    }
+
+    public AutoTree then(AutoTree node) {
+        next = node;
+        next.prev = this;
+        return next;
+    }
+
+    public AutoTree then(String... variant) {
+        next = new AutoTree(variant);
+        next.prev = this;
+        return next;
+    }
+
+    @Override
+    public String toString() {
+
+        String[] lll = new String[variants.size()];
+        for (int i = 0; i < variants.size(); i++) {
+            lll[i] = variants.get(i);
+        }
+
+        return Arrays.toString(lll);
+    }
+
+    private static AutoTree getRoot(AutoTree node) {
+        final AutoTree prev = node.prev;
+        if (prev == null) return node;
+        return getRoot(prev);
+    }
+
+    public static List<AutoTree> or(AutoTree... branches) {
+        for (int i = 0; i < branches.length; i++) {
+            branches[i] = getRoot(branches[i]);
+        }
+        return Arrays.asList(branches);
+    }
+
+    public static AutoTree branch(String variant) {
+        return new AutoTree(variant);
+    }
+
+    public static AutoTree array(String... variant) {
+        return new AutoTree(variant);
+    }
+}
+
---
+++ src/test/java/AutoTreeTests.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+import java.util.List;
+
+import su.levenetc.playground.java.datastructures.AutoTree;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static su.levenetc.playground.java.datastructures.AutoTree.array;
+import static su.levenetc.playground.java.datastructures.AutoTree.branch;
+import static su.levenetc.playground.java.datastructures.AutoTree.or;
+
+public class AutoTreeTests {
+    @Test
+    public void test01() {
+        final List<AutoTree> roots = or(
+                branch("select")
+                        .then("user")
+                        .then(array("name", "age"))
+                        .then("where"),
+                branch("delete from")
+        );
+
+        AutoTree selectRoot = roots.get(0);
+        assertNotNull(selectRoot.isCompletable("se"));
+        assertNull(selectRoot.isCompletable("x"));
+
+        AutoTree user = selectRoot.next();
+        assertNotNull(user.isCompletable("us"));
+
+        AutoTree fields = user.next();
+        assertNotNull(fields.isCompletable("n"));
+        assertNotNull(fields.isCompletable("age"));
+    }
+}
