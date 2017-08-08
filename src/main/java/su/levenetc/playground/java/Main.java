ada3fc4a04c4888e068f14e6a447e62eb69b8e30
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-08-08 07:31:40 +0200

    Move Main.java to gitignore

--- src/main/java/su/levenetc/playground/java/Main.java
+++ src/main/java/su/levenetc/playground/java/Main.java
@@ -1,13 +1,29 @@
 package su.levenetc.playground.java;
 
+import java.util.PriorityQueue;
+
 import su.levenetc.playground.java.utils.Out;
+import su.levenetc.playground.java.utils.ThreadsUtils;
 
 public class Main {
 
     public static void main(String[] args) {
-        Out.pln(5 / 2);
-        Out.pln(3 / 2);
-        Out.pln(4 / 2);
+        ThreadsUtils.sleep(50);
+        Out.pln();
+        run();
+        Out.pln();
+        ThreadsUtils.sleep(50);
+    }
+
+    static void run() {
+
+        Out.pln(5/2);
+        Out.pln(3/2);
+
+    }
+
+    static boolean isEven(int value) {
+        return (value & 1) == 0;
     }
 
 }
