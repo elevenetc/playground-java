81a5717ff855acdbf6de2cd0c2f5dd3efc7808e7
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-08-08 07:28:17 +0200

    Remove rotate matrix duplicate

--- src/test/java/Rotate2DMatrixTests.java
+++ src/test/java/Rotate2DMatrixTests.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 import su.levenetc.playground.java.utils.Utils;
 
 import static org.junit.Assert.assertArrayEquals;
-import static su.levenetc.playground.java.algs.Rotate2DMatrix.rotate90Clockwise;
+import static su.levenetc.playground.java.algs.RotateMatrix.rotate;
 
 public class Rotate2DMatrixTests {
     @Test
@@ -12,7 +12,7 @@ public class Rotate2DMatrixTests {
                 new int[]{7, 4, 1},
                 new int[]{8, 5, 2},
                 new int[]{9, 6, 3}
-        }, rotate90Clockwise(new int[][]{
+        }, rotate(new int[][]{
                 new int[]{1, 2, 3},
                 new int[]{4, 5, 6},
                 new int[]{7, 8, 9}
@@ -26,7 +26,7 @@ public class Rotate2DMatrixTests {
                 new int[]{14, 10, 6, 2},
                 new int[]{15, 11, 7, 3},
                 new int[]{16, 12, 8, 4}
-        }, rotate90Clockwise(new int[][]{
+        }, rotate(new int[][]{
                 new int[]{1, 2, 3, 4},
                 new int[]{5, 6, 7, 8},
                 new int[]{9, 10, 11, 12},
@@ -42,7 +42,7 @@ public class Rotate2DMatrixTests {
                 new int[]{23, 18, 13, 8, 3},
                 new int[]{24, 19, 14, 9, 4},
                 new int[]{25, 20, 15, 10, 5}
-        }, rotate90Clockwise(new int[][]{
+        }, rotate(new int[][]{
                 Utils.arrayRange(1, 5),
                 Utils.arrayRange(6, 10),
                 Utils.arrayRange(11, 15),
