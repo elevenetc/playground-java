35137924de8a857f91fccdfca2ac9d7584f08cbd
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-11-09 09:31:18 +0100

    Update alg: intersection of two linked lists

--- src/main/java/su/levenetc/playground/java/algs/IntersectionOfTwoLinkedLists.java
+++ src/main/java/su/levenetc/playground/java/algs/IntersectionOfTwoLinkedLists.java
@@ -6,7 +6,8 @@ import java.util.LinkedList;
 import su.levenetc.playground.java.datastructures.IntNode;
 
 public class IntersectionOfTwoLinkedLists {
-    static IntNode get(IntNode listA, IntNode listB) {
+
+    static IntNode getMem(IntNode listA, IntNode listB) {
         Deque<IntNode> stackA = fillStack(listA);
         Deque<IntNode> stackB = fillStack(listB);
         IntNode intersection = null;
@@ -23,12 +24,75 @@ public class IntersectionOfTwoLinkedLists {
         return intersection;
     }
 
-    static Deque<IntNode> fillStack(IntNode node) {
+    private static Deque<IntNode> fillStack(IntNode node) {
         Deque<IntNode> result = new LinkedList<>();
         while (node != null) {
             result.push(node);
             node = node.next;
         }
         return result;
+    }
+
+    static IntNode getWithoutMem(IntNode listA, IntNode listB) {
+
+        ListData dataA = length(listA);
+        ListData dataB = length(listB);
+
+        if (dataA.last != dataB.last) return null;
+
+        IntNode longer;
+        IntNode shorter;
+        ListData longData;
+        ListData shortData;
+
+        if (dataA.length > dataB.length) {
+            longer = listA;
+            shorter = listB;
+            longData = dataA;
+            shortData = dataB;
+        } else {
+            longer = listB;
+            shorter = listA;
+            longData = dataB;
+            shortData = dataA;
+        }
+
+        longer = getKthNode(longer, longData.length - shortData.length);
+
+        while (longer != shorter) {
+            longer = longer.next;
+            shorter = shorter.next;
+        }
+
+        return longer;
+    }
+
+    private static IntNode getKthNode(IntNode node, int k) {
+        while (k != 0) {
+            node = node.next;
+            k--;
+        }
+        return node;
+    }
+
+    private static ListData length(IntNode node) {
+        int length = 0;
+        IntNode last = null;
+        while (node != null) {
+            length++;
+            last = node;
+            node = node.next;
+        }
+        return new ListData(length, last);
+    }
+
+    static class ListData {
+        int length;
+        IntNode last;
+
+        public ListData(int length, IntNode last) {
+            this.length = length;
+            this.last = last;
+        }
     }
 }
--- src/test/java/su/levenetc/playground/java/algs/IntersectionOfTwoLinkedListsTests.java
+++ src/test/java/su/levenetc/playground/java/algs/IntersectionOfTwoLinkedListsTests.java
@@ -4,18 +4,29 @@ import org.junit.Test;
 
 import su.levenetc.playground.java.datastructures.IntNode;
 
-import static su.levenetc.playground.java.algs.IntersectionOfTwoLinkedLists.get;
+import static su.levenetc.playground.java.algs.IntersectionOfTwoLinkedLists.getMem;
+import static su.levenetc.playground.java.algs.IntersectionOfTwoLinkedLists.getWithoutMem;
 import static su.levenetc.playground.java.utils.TestUtils.assertEquals;
 
 public class IntersectionOfTwoLinkedListsTests {
     @Test
-    public void test() {
+    public void test01() {
         IntNode listA = IntNode.list(0, 1, 2);
         IntNode listB = IntNode.list(0, 1, 2);
         IntNode interNode = new IntNode(100);
         listA.next = interNode;
         listB.next = interNode;
         interNode.next = new IntNode(200);
-        assertEquals(interNode, get(listA, listB));
+        assertEquals(interNode, getMem(listA, listB));
+        assertEquals(interNode, getWithoutMem(listA, listB));
     }
+
+    @Test
+    public void test02() {
+        IntNode listA = IntNode.list(0, 1, 2);
+        IntNode listB = IntNode.list(0, 1, 2);
+        assertEquals(null, getMem(listA, listB));
+        assertEquals(null, getWithoutMem(listA, listB));
+    }
+
 }
