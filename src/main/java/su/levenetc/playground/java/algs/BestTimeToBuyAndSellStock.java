f3baf9a2e7add12402254b24e3680da648e9999b
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-12-16 23:44:21 +0100

    Add alg: best time to buy and sell stock

---
+++ src/main/java/su/levenetc/playground/java/algs/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,15 @@
+package su.levenetc.playground.java.algs;
+
+public class BestTimeToBuyAndSellStock {
+    public static int maxProfit(int prices[]) {
+        int minprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minprice)
+                minprice = prices[i];
+            else if (prices[i] - minprice > maxprofit)
+                maxprofit = prices[i] - minprice;
+        }
+        return maxprofit;
+    }
+}
