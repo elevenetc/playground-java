f147b78a73f23b6fe0ebb08738d689cef5129b28
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-12-24 21:49:07 +0100

    Update random BST

--- src/main/java/su/levenetc/playground/java/algs/trees/RandomBST.java
+++ src/main/java/su/levenetc/playground/java/algs/trees/RandomBST.java
@@ -5,79 +5,53 @@ public class RandomBST {
     Node root;
 
     public void insert(int value) {
-
         if (root == null) {
             root = new Node(value);
-            return;
-        }
-
-        Node curr = root;
-        while (true) {
-            curr.size++;
-            if (value <= curr.value) {
-                if (curr.left == null) {
-                    curr.left = new Node(value);
-                    return;
-                } else {
-                    curr = curr.left;
-                }
-            } else {
-                if (curr.right == null) {
-                    curr.right = new Node(value);
-                    return;
-                } else {
-                    curr = curr.right;
-                }
-            }
-        }
-    }
-
-    public Node find(int value) {
-        Node curr = root;
-        while (curr != null) {
-            if (value == curr.value) {
-                return curr;
-            } else if (value < curr.value) {
-                curr = curr.left;
-            } else {
-                curr = curr.right;
-            }
-        }
-        return null;
-    }
-
-    public Node getRandomNode() {
-        if (root == null) return null;
-        //int rnd = (int) (Math.random() * root.size);
-        int rnd = 4;
-        return getRandomNode(root, rnd);
-    }
-
-    private Node getRandomNode(Node node, int num) {
-        int leftSize = (node.left == null) ? 0 : node.left.size;
-        if (num < leftSize) {
-            return getRandomNode(node.left, num);
-        } else if (num == leftSize) {
-            return node;
         } else {
-            return getRandomNode(node.right, num - leftSize - 1);
+            insert(root, value);
+        }
+    }
+
+    public int getRandom() {
+        int rnd = (int) (Math.random() * (root.size + 1));
+        return getRandom(root, rnd);
+    }
+
+    private int getRandom(int rnd) {
+        return getRandom(root, rnd);
+    }
+
+    private int getRandom(Node node, int rnd) {
+        int left = node.left == null ? 0 : (node.left.size + 1);
+        if (rnd < left) {
+            return getRandom(node.left, rnd);
+        } else if (rnd == left) {
+            return node.value;
+        } else {
+            return getRandom(node.right, rnd - left - 1);
+        }
+    }
+
+    private void insert(Node root, int value) {
+        root.size++;
+        if (value < root.value) {
+            if (root.left == null) root.left = new Node(value);
+            else insert(root.left, value);
+        } else {
+            if (root.right == null) root.right = new Node(value);
+            else insert(root.right, value);
         }
     }
 
     static class Node {
 
+        int value;
+        int size;
         Node left;
         Node right;
-        int value;
-        int size = 1;
 
         public Node(int value) {
             this.value = value;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("(v:%d s:%d)", value, size);
         }
     }
 }
