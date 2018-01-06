f90040cb109151bb6afa1e69269345bf6229dd0f
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2018-01-06 02:27:07 +0100

    Update climbing stairs

--- src/main/java/su/levenetc/playground/java/algs/ClimbingSteps.java
+++ src/main/java/su/levenetc/playground/java/algs/ClimbingSteps.java
@@ -7,6 +7,10 @@ import java.util.Map;
  * Created by eugene.levenetc on 11/07/2017.
  */
 public class ClimbingSteps {
+
+    /**
+     * O(n ^ 2)
+     */
     public static int get(int steps) {
         if (steps <= 2) return steps;
         return getInternal(steps, new HashMap<>());
@@ -18,8 +22,25 @@ public class ClimbingSteps {
 
         if (mem.containsKey(steps)) return mem.get(steps);
 
-        final int result = getInternal(steps - 1, mem) + getInternal(steps - 2, mem);
+        int result = getInternal(steps - 1, mem) + getInternal(steps - 2, mem);
         mem.put(steps, result);
         return result;
+    }
+
+    /**
+     * O(n)
+     */
+    public static int get2(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
     }
 }
--- src/test/java/ClimbingStepsTests.java
+++ src/test/java/ClimbingStepsTests.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static su.levenetc.playground.java.algs.ClimbingSteps.get;
+import static su.levenetc.playground.java.algs.ClimbingSteps.*;
 
 /**
  * Created by eugene.levenetc on 11/07/2017.
@@ -10,15 +10,11 @@ public class ClimbingStepsTests {
     @Test
     public void test01() {
         assertEquals(8, get(5));
-    }
-
-    @Test
-    public void test02() {
         assertEquals(2, get(2));
-    }
-
-    @Test
-    public void test03() {
         assertEquals(3, get(3));
+
+        assertEquals(8, get2(5));
+        assertEquals(2, get2(2));
+        assertEquals(3, get2(3));
     }
 }
