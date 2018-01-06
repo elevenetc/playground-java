fd55f7c835129154cced170628811f6989887237
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2018-01-06 05:49:57 +0100

    Update house robbery

--- src/main/java/su/levenetc/playground/java/algs/dp/HouseRobbery.java
+++ src/main/java/su/levenetc/playground/java/algs/dp/HouseRobbery.java
@@ -5,34 +5,52 @@ import java.util.Map;
 
 public class HouseRobbery {
 
-    public static int robWithoutMemory(int... nums) {
-        int prevMax = 0;
-        int currMax = 0;
-        for (int x : nums) {
-            int temp = currMax;
-            currMax = Math.max(prevMax + x, currMax);
-            prevMax = temp;
-        }
-        return currMax;
-    }
-
-    public static int rob(int... nums) {
+    public static int rob1(int... nums) {
         if (nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
-        return robInt(0, new HashMap<>(), nums);
+        else if (nums.length == 1) return nums[0];
+        else return robInt(0, new HashMap<>(), nums);
     }
 
     static int robInt(int i, Map<Integer, Integer> mem, int... houses) {
         if (i > houses.length - 1) return 0;
+        else if (mem.containsKey(i)) return mem.get(i);
 
-        if (mem.containsKey(i)) return mem.get(i);
-
-        int r0 = houses[i];
         int r1 = robInt(i + 1, mem, houses);
-        int r2 = robInt(i + 2, mem, houses);
+        int r2 = houses[i] + robInt(i + 2, mem, houses);
 
-        int max = Math.max(r0 + r2, r1);
+        int max = Math.max(r1, r2);
         mem.put(i, max);
         return max;
     }
+
+    //
+
+    public static int rob2(int... nums) {
+        if (nums == null || nums.length == 0) return 0;
+        else if (nums.length == 1) return nums[0];
+
+        int[] m = new int[nums.length];
+        m[0] = nums[0];
+        m[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++)
+            m[i] = Math.max(m[i - 1], m[i - 2] + nums[i]);
+
+        return m[nums.length - 1];
+    }
+
+    //
+
+    public static int rob3(int... nums) {
+        int h0 = nums[0];
+        int h1 = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            int current = nums[i];
+            int prev = h1;
+            h1 = Math.max(h0 + current, h1);
+            h0 = prev;
+        }
+        return h1;
+    }
+
 }
--- src/test/java/su/levenetc/playground/java/algs/HouseRobberyTests.java
+++ src/test/java/su/levenetc/playground/java/algs/HouseRobberyTests.java
@@ -2,15 +2,26 @@ package su.levenetc.playground.java.algs;
 
 import org.junit.Test;
 
-import su.levenetc.playground.java.algs.dp.HouseRobbery;
-
 import static org.junit.Assert.assertEquals;
+import static su.levenetc.playground.java.algs.dp.HouseRobbery.rob1;
+import static su.levenetc.playground.java.algs.dp.HouseRobbery.rob2;
+import static su.levenetc.playground.java.algs.dp.HouseRobbery.rob3;
+import static su.levenetc.playground.java.utils.Utils.array;
 
 public class HouseRobberyTests {
     @Test
-    public void test() {
+    public void testLarge() {
         int[] houses = new int[]{104, 209, 137, 52, 158, 67, 213, 86, 141, 110, 151, 127, 238, 147, 169, 138, 240, 185, 246, 225, 147, 203, 83, 83, 131, 227, 54, 78, 165, 180, 214, 151, 111, 161, 233, 147, 124, 143};
-        assertEquals(3176, HouseRobbery.rob(houses));
-        assertEquals(3176, HouseRobbery.robWithoutMemory(houses));
+        assertEquals(3176, rob1(houses));
+        assertEquals(3176, rob2(houses));
+        assertEquals(3176, rob3(houses));
+    }
+
+    @Test
+    public void test(){
+        int[] houses = array(1, 2, 1, 3, 6);
+        assertEquals(8, rob1(houses));
+        assertEquals(8, rob2(houses));
+        assertEquals(8, rob3(houses));
     }
 }
