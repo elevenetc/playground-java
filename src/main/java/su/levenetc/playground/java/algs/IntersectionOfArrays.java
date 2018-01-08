ccc0c4dbb81495833e0bd8c1e24870b19ac29f49
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2018-01-08 18:55:06 +0100

    Update two sum

--- src/main/java/su/levenetc/playground/java/algs/IntersectionOfArrays.java
+++ src/main/java/su/levenetc/playground/java/algs/IntersectionOfArrays.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class IntersectionOfArrays {
+
     static List<Integer> get(int[] a, int[] b) {
         Map<Integer, Integer> map = new HashMap<>();
         int[] array;
--- src/main/java/su/levenetc/playground/java/algs/TwoSum.java
+++ src/main/java/su/levenetc/playground/java/algs/TwoSum.java
@@ -7,7 +7,8 @@ import java.util.Map;
  * https://leetcode.com/problems/two-sum/description/
  */
 public class TwoSum {
-    public static int[] get(int[] nums, int sum) {
+
+    public static int[] get1(int[] nums, int sum) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = sum - nums[i];
@@ -18,4 +19,27 @@ public class TwoSum {
         }
         return null;
     }
+
+    public static int[] get2(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int a = 0;
+        int b = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            int complement = target - num;
+            if (map.containsKey(complement)) {
+                a = i;
+                b = map.get(complement);
+                break;
+            } else {
+                map.put(num, i);
+            }
+        }
+        return new int[]{a, b};
+
+    }
+
 }
