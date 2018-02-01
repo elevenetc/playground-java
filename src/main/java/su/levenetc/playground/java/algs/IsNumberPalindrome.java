7003a84a6c2aad2be2beb606be6dfb9411bbbe85
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2018-02-01 12:14:42 +0100

    Update is palindrome number

--- src/main/java/su/levenetc/playground/java/algs/IsNumberPalindrome.java
+++ src/main/java/su/levenetc/playground/java/algs/IsNumberPalindrome.java
@@ -1,15 +1,17 @@
 package su.levenetc.playground.java.algs;
 
 public class IsNumberPalindrome {
-    static boolean isNumberPalindrome(int n) {
-        int v = 0;
-        while (n > v) {
-            v *= 10;
-            v += n % 10;
-            if (v == n) return true;
+    public boolean IsPalindrome(int n) {
+        //if numbers ends with zero then it's not palindrome
+        if (n < 0 || (n % 10 == 0 && n != 0)) return false;
+
+        int inverted = 0;
+        while (n > inverted) {
+            inverted = inverted * 10 + n % 10;
             n /= 10;
         }
 
-        return n == v;
+        //division by 10 is for case odd amount of numbers: "12521" > "12 != 125" > "125/10=12" > "12 == 12"
+        return n == inverted || n == inverted / 10;
     }
 }
